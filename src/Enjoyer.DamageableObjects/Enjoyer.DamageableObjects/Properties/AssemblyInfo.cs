52e254e708f0007596d080f8178d54a2921cadda
Enjoyer <134187582+enjoyer-cs@users.noreply.github.com>
2025-05-03 21:28:15 +0300

    Create project

---
+++ src/Enjoyer.DamageableObjects/Enjoyer.DamageableObjects/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using Enjoyer.DamageableObjects;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Enjoyer.DamageableObjects")]
+[assembly: AssemblyDescription("An Exiled plugin that allows to use damageable gameobjects in SCP: Secret Laboratory")]
+[assembly: AssemblyProduct("DamageableObjects")]
+[assembly: AssemblyCopyright("Copyright © enjoyer-cs 2025")]
+[assembly: AssemblyCulture("ru-RU")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("D2018C81-0B4F-4103-9932-3459CD2300AE")]
+
+[assembly: AssemblyVersion(ProjectInfo.Version)]
+[assembly: AssemblyFileVersion(ProjectInfo.Version)]
