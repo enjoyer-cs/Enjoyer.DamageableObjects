258a49a7546db9ec09bdb67381b36864773da06d
Enjoyer <134187582+enjoyer-cs@users.noreply.github.com>
2025-05-03 23:36:56 +0300

    Create ProjectInfo.cs

---
+++ src/Enjoyer.DamageableObjects/Enjoyer.DamageableObjects/ProjectInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Enjoyer.DamageableObjects;
+
+/// <summary>
+///     Информация о проекте <see cref="Enjoyer.DamageableObjects" />
+/// </summary>
+public static class ProjectInfo
+{
+    /// <summary>
+    ///     Текущая версия
+    /// </summary>
+    public const string Version = "1.0.0";
+}
