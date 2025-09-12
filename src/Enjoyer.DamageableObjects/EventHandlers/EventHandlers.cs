e56e296b3302935149062a0d1573ae1e93c7a3dd
Enjoyer-cs <Enjoyer.cs@outlook.com>
2025-09-12 17:05:58 +0300

    Revert "Delete Support for Door Damage"
    This reverts commit 30d28e4952e9a13be2f431ce194e727fb579f9b4.

--- src/Enjoyer.DamageableObjects/EventHandlers/EventHandlers.cs
+++ src/Enjoyer.DamageableObjects/EventHandlers/EventHandlers.cs
@@ -1,21 +1,44 @@
-﻿using Enjoyer.DamageableObjects.Patches.Scp096;
-using LabApi.Events.Arguments.Scp096Events;
-using LabApi.Events.Handlers;
-using PlayerRoles.PlayableScps.Scp096;
+﻿using Enjoyer.DamageableObjects.API.Components;
+using Enjoyer.DamageableObjects.API.Extensions;
+using Enjoyer.DamageableObjects.Configs;
+using Enjoyer.DamageableObjects.Patches.Scp096;
+using Exiled.API.Enums;
+using Exiled.API.Features.Doors;
+using Exiled.Events.EventArgs.Scp096;
+using System.Collections.Generic;
+using System.Linq;
+using MapEvents = Exiled.Events.Handlers.Map;
+using Scp096Events = Exiled.Events.Handlers.Scp096;
 
-namespace Enjoyer.DamageableObjects.EventHandlers;
+namespace Enjoyer.DamageableObjects;
 
 internal class EventHandlers
 {
     internal virtual void RegisterEvents()
     {
+        MapEvents.Generated += OnGenerated;
         Scp096Events.Charging += OnCharging;
     }
 
     internal virtual void UnregisterEvents()
     {
+        MapEvents.Generated -= OnGenerated;
         Scp096Events.Charging -= OnCharging;
     }
 
-    private static void OnCharging(Scp096ChargingEventArgs ev) => ProcessHitsPatch._chargeAttackedComponents.Remove((Scp096Role)ev.Player.RoleBase);
+    private static void OnGenerated()
+    {
+        foreach (KeyValuePair<DoorType, DamageableDoorsProperties> pair in DoPlugin.PluginConfig.DamageableDoorTypes)
+        {
+            foreach (BreakableDoor door in Door.List.Where(door => door.Type == pair.Key).Select(door => door.As<BreakableDoor>()))
+            {
+                DamageableDoor component = door.GameObject.AddDamageableComponent<DamageableDoor>(pair.Value);
+                component.Door = door;
+                component.NotAffectToDamage = pair.Value.NotAffectToDamage;
+                component.HitMarkerSize = DoPlugin.PluginConfig.DoorHitMarkerSize;
+            }
+        }
+    }
+
+    private static void OnCharging(ChargingEventArgs ev) => ProcessHitsPatch._chargeAttackedComponents.Remove(ev.Scp096.Base);
 }
